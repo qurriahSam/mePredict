6c1ea723b3ca3db6b707931f02f73d77883d5e1d
90150N <mugirethe1@gmail.com>
2022-06-02 17:21:53 +0300

    add 0 infront of dates less than 10

--- JS/app.js
+++ JS/app.js
@@ -4,13 +4,14 @@ let fixtures;
 const getCurrentDate = () => {
   const todayCalender = new Date();
   const date = todayCalender.getDate().toString();
+  const finalDate = date.length === 1 ? "0" + date : date;
   const month = (() =>
     todayCalender.getMonth() + 1 < 10
       ? `0${(todayCalender.getMonth() + 1).toString()}`
       : todayCalender.getMonth() + 1)();
   const year = todayCalender.getFullYear().toString();
 
-  return `${year}-${month}-${date}`;
+  return `${year}-${month}-${finalDate}`;
 };
 let today = getCurrentDate();
 console.log(today);
